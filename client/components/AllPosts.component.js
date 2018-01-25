351cfcf75f5d5800629a4d2cd559f45498dfbbef
ankurkoul <er.ankurkoul@gmail.com>
2018-01-25 17:21:38 +0530

    first commit

---
+++ client/components/AllPosts.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PostDetails from './PostDetails.component';
+export default class AllPosts extends React.Component {
+    render() {
+        console.log('Within Render-> AllPosts')
+        console.log(this.props.myposts);
+        var list = this.props.myposts.map(
+            (e, i) =>
+                <PostDetails post={e} key={i}
+                    index={i} {...this.props} />)
+            ;
+        return <div>
+
+            {list}
+        </div>
+    }
+}
